746f0d87ae0a3197f30a2370f747e794343b646e
adinugraha2611 <m.adinugraha.ug@gmail.com>
2020-11-04 15:03:40 +0700

    edit knexFile.js

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,19 @@
-// Update with your config settings.
-
+require('dotenv').config();
 module.exports = {
   development: {
-    client: "pg",
+    client: 'pg',
     connection: {
-      host: "localhost",
-      user: "nugie",
-      password: "nuganteng",
-      database: "request-epub",
+      host: 'localhost',
+      user: 'nugie',
+      password: 'nuganteng',
+      database: 'request-epub',
     },
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  },
   //   pool: {
   //     min: 2,
   //     max: 10
